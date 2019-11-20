011943f9dabc3d6de4cd1f9eb12f02270bf6f5e2
raynier lara <raynier98@hotmail.es>
2019-11-20 19:52:54 -0400

    facebook

---
+++ 2doparc/2doparc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2doparc.Startup))]
+namespace _2doparc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
